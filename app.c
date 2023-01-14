d6cbc43c60ba50c00acd397de30c8e847dd4ce8e
michael <michael@DebianCoding.myguest.virtualbox.org>
2023-01-13 19:25:50 -0500

    Created clang-format file and formatted C and H source files.

--- app.c
+++ app.c
@@ -1,37 +1,40 @@
 
 // Michael Rees
 // Operating Systems - Remove Directory Application
-// This program will prompt a user for a directory to delete then delete the directory contents.
+// This program will prompt a user for a directory to delete then delete the
+// directory contents.
 
-#include <stdio.h> 
-#include <string.h>
+#include "remove_dir/remove_dir.h"
 #include <dirent.h>
 #include <stdbool.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
-#include "remove_dir/remove_dir.h"
 
 int main() {
-    char * folderName = malloc(50);
-    char * choice = malloc(1);
+  char *folderName = malloc(50);
+  char *choice = malloc(1);
 
-    printf("Remove Directory App\n");
-    printf("Enter the folder name:");
-    scanf("%s", folderName);
+  printf("Remove Directory App\n");
+  printf("Enter the folder name:");
+  scanf("%s", folderName);
 
-    printf("You Entered:%s.\nAre you sure you want to delete it and its contents? (y for yes, n for no)\n", folderName);
-    scanf("%s", choice);
+  printf("You Entered:%s.\nAre you sure you want to delete it and its "
+         "contents? (y for yes, n for no)\n",
+         folderName);
+  scanf("%s", choice);
 
-    bool deletionConfirmed = choice[0] == 'y';
+  bool deletionConfirmed = choice[0] == 'y';
 
-    if (deletionConfirmed) {
-        delete_requested_folder(folderName);
-    } else {
-        notify_user_cancellation();
-    }
+  if (deletionConfirmed) {
+    delete_requested_folder(folderName);
+  } else {
+    notify_user_cancellation();
+  }
 
-    free(folderName);
-    free(choice);
+  free(folderName);
+  free(choice);
 
-    return 0;
-} 
+  return 0;
+}
--- remove_dir/remove_dir.h
+++ remove_dir/remove_dir.h
@@ -3,12 +3,12 @@
 
 #include <stdbool.h>
 
-char * create_path(char * path, char * filename);
-bool is_valid_dir(char * currentPath);
-void delete_dir(char * dirPath);
-void delete_file(char * dirFile);
-void empty_dir(char * currentPath);
-void delete_requested_folder(char * folderName);
+char *create_path(char *path, char *filename);
+bool is_valid_dir(char *currentPath);
+void delete_dir(char *dirPath);
+void delete_file(char *dirFile);
+void empty_dir(char *currentPath);
+void delete_requested_folder(char *folderName);
 void notify_user_cancellation();
 
 #endif
